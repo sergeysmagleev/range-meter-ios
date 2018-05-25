e608b89272ab2ea956a3fa987a49f8aa55337762
Dylan Maryk <dylan@door2door.io>
2018-05-25 18:29:11 +0200

    Request isoline

--- RangeMeter/RangeMeter/ViewController.swift
+++ RangeMeter/RangeMeter/ViewController.swift
@@ -6,20 +6,21 @@
 //  Copyright © 2018 Sergey Smagleev. All rights reserved.
 //
 
+import RxAlamofire
+import RxSwift
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        RxAlamofire.requestJSON(.get, "https://isoline.route.cit.api.here.com/routing/7.2/calculateisoline.json?app_id=7IQiOdNho9z1vWo9aECh&app_code=oQDeGdXmm4oQAwqlwnCouQ&mode=fastest;car&rangetype=time&start=geo!52.51578,13.37749&range=300&singlecomponent=true")
+            .subscribe(onNext: { (r, json) in
+                
+            })
+            .disposed(by: disposeBag)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
-
